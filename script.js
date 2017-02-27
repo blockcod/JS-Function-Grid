de085e49b86956462e3df42d445664b5c39506d0
Josue <jescamilla@aclanyc.com>
2017-02-27 13:21:53 +0000

    color grid

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Write your function calls below this line
+setColor(0, 0, "black");
+setColor(9, 9, "black");
+setColor(0, 9, "black");
+setColor(9, 0, "black")
+setColor(2, 3, "yellow");
+setColor(2, 6, "yellow");
+setColor(5, 2, "yellow");
+setColor(4, 3, "yellow");
+setColor(5, 3, "yellow");
+setColor(5, 4, "yellow");
+setColor(5, 5, "yellow");
+setColor(5, 6, "yellow");
+setColor(4, 6, "yellow");
+setColor(5, 7, "yellow");
